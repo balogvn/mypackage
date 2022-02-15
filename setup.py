f78a263dff87235625f4b60a1727bc7c07640fa1
balogvn <balogvn@gmail.com>
2022-02-15 11:56:13 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/balogvn/mypackage', 
+    author='Kayode Balogun',
+    author_email='balogvn@gmail.com'
+
+
+)
